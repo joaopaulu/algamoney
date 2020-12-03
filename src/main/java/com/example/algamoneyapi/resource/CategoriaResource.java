6f21fcbc6b25c72bf87390299e551bb597cf1fde
joaopaulu <jptick@gmail.com>
2020-12-03 18:38:41 -0300

    Add Listener Event

--- src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
@@ -1,24 +1,18 @@
 package com.example.algamoneyapi.resource;
 
-import java.net.URI;
-import java.util.List;
-import java.util.Optional;
+import com.example.algamoneyapi.event.RecursoCriadoEvent;
+import com.example.algamoneyapi.model.Categoria;
+import com.example.algamoneyapi.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-
-import com.example.algamoneyapi.model.Categoria;
-import com.example.algamoneyapi.repository.CategoriaRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
@@ -26,6 +20,9 @@ public class CategoriaResource {
 
   @Autowired
   private CategoriaRepository categoriaRepository;
+
+  @Autowired
+  private ApplicationEventPublisher publisher;
 
   @GetMapping
   public List<Categoria> listar() {
@@ -35,12 +32,8 @@ public class CategoriaResource {
   @PostMapping
   public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
     Categoria categoriaSalva = categoriaRepository.save(categoria);
-
-    URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-        .buildAndExpand(categoriaSalva.getCodigo()).toUri();
-    response.setHeader("Location", uri.toASCIIString());
-
-    return ResponseEntity.created(uri).body(categoriaSalva);
+    publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
+    return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
   }
 
   @GetMapping("/{codigo}")
